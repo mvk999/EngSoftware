56bf252f4bac2f1ccedefc68678a79df07381514
Arthur Soares <arthursoaresmarques78@gmail.com>
2025-08-30 11:42:43 -0300

    Estrutura da tela de login

--- EngSoftware/src/routes/Tela_Login/login.jsx
+++
@@ -1,9 +0,0 @@
-import './login.css'
-function Login() {
-  return (
-    <div>
-    </div>
-  )
-}
-
-export default Login
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from './routes/Tela_Login/login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 )
