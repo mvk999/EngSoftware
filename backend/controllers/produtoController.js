86badd06a0bf9213b5ad04628a311968eabc35c8
Diego Oliveira <diegoalves.div@gmail.com>
2025-12-02 19:00:21 -0300

    fix: [Diego] alterando duplicação de função

--- backend/controllers/produtoController.js
+++ backend/controllers/produtoController.js
@@ -11,20 +11,6 @@ async function getProdutos(req, res) {
     return res.status(500).json({ message: "Erro ao buscar produtos." });
   }
 }
-async function getProduto(req, res) {
-  try {
-    const { id } = req.params;
-
-    const produto = await produtoServices.getProduto(id);
-    if (!produto)
-      return res.status(404).json({ message: "Produto não encontrado." });
-
-    return res.status(200).json(produto);
-  } catch (err) {
-    console.error("produtoController.getProduto:", err);
-    return res.status(500).json({ message: "Erro ao buscar produto." });
-  }
-}
 
 
 async function getProduto(req, res) {
